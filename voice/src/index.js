75c09eb8e02a0acab1414ca41594c3df7f8f07e9
Harshavardhanjakku <2203a51012@sru.edu.in>
2024-11-26 13:57:38 +0530

    Updated the Biologygame

--- voice/src/index.js
+++ voice/src/index.js
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import Mathapp from './Mathgame';
+import Bioapp from './Biologygame';
 import reportWebVitals from './reportWebVitals';
+import Bioapp from './Biologygame';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
   <App />
     <Mathapp/>
+    <Bioapp/>
   </React.StrictMode>
 );
 
